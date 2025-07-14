018a1e3da66611a96a97249740b8b264682c3a06
Murugan Ganesan <rushmurugan@gmail.com>
2025-07-14 15:11:41 +0530

    Update upi.py

--- upi.py
+++ upi.py
@@ -22,7 +22,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.markdown('<h1 class="main-title"> SmartSpend </h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="main-title"> SmartSpend AI </h1>', unsafe_allow_html=True)
 st.markdown('<p class="sub-title"> </p>', unsafe_allow_html=True)
 
 st.sidebar.title("-How to Use This Tool?-")
